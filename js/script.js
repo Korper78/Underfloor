16e91714561cc956756db30de792d513a5b3fa88
Korper78 <korper78@gmail.com>
2023-03-17 12:42:51 +0300

    day4, calculator+buttons

--- js/script.js
+++ js/script.js
@@ -18,3 +18,39 @@ const swiper = new Swiper('.hero__slider', {
     },
   },
 });
+
+//calculator
+
+const calcForm = document.querySelector('.js-calc-form');
+const totalSquare = document.querySelector('.js-square');
+const totalPrice = document.querySelector('.js-total-price');
+const result = document.querySelector('.calc__result-wrapper');
+const resultButton = document.querySelector('.calc__submit');
+const orderButton = document.querySelector('.header__button');
+
+const tariff = {
+  economy: 550,
+  comfort: 1400,
+  premium: 2700,
+}
+
+calcForm.addEventListener('input', (e) => {
+  if (calcForm.width.value > 0 && calcForm.length.value > 0) {
+    resultButton.disabled = false;
+  };
+});
+
+calcForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  if (calcForm.width.value > 0 && calcForm.length.value > 0) {
+    const square = calcForm.width.value * calcForm.length.value;
+    const price = square * tariff[calcForm.tariff.value];
+    
+    result.style.display = 'block';
+    totalSquare.textContent = `${square}  кв.м`;
+    totalPrice.textContent = `${price} руб`
+
+    orderButton.disabled = false;
+  };
+
+});
