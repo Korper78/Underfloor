8849b8fd5a481c3c7a45ecfa3b569098d0e5809b
Korper78 <korper78@gmail.com>
2023-03-20 12:31:54 +0300

    day5, modal

--- js/script.js
+++ js/script.js
@@ -35,9 +35,12 @@ const tariff = {
 }
 
 calcForm.addEventListener('input', (e) => {
-  if (calcForm.width.value > 0 && calcForm.length.value > 0) {
-    resultButton.disabled = false;
-  };
+  resultButton.disabled = !(calcForm.width.value > 0 && calcForm.length.value > 0);
+  // if (calcForm.width.value > 0 && calcForm.length.value > 0) {
+  //   resultButton.disabled = false;
+  // } else {
+  //   resultButton.disabled = true;
+  // };
 });
 
 calcForm.addEventListener('submit', (event) => {
@@ -53,4 +56,80 @@ calcForm.addEventListener('submit', (event) => {
     orderButton.disabled = false;
   };
 
+});
+
+
+// модальное окно
+const scrollController = {
+  scrollPosition: 0,
+  disabledScroll() {
+    scrollController.scrollPosition = window.scrollY;
+    document.body.style.cssText = `
+      overflow: hidden;
+      position: fixed;
+      top: -${scrollController.scrollPosition}px;
+      left: 0;
+      height: 100vh;
+      width: 100vw;
+      padding-right: ${window.innerWidth - document.body.offsetWidth}px
+    `;
+    document.documentElement.style.scrollBehavior = 'unset';
+  },
+  enabledScroll() {
+    document.body.style.cssText = '';
+    window.scroll({top: scrollController.scrollPosition})
+    document.documentElement.style.scrollBehavior = '';
+  },
+}
+
+
+const modalController = ({modal, btnOpen, btnClose, time = 300}) => {
+  const buttonElems = document.querySelectorAll(btnOpen);
+  const modalElem = document.querySelector(modal);
+
+  modalElem.style.cssText = `
+    display: flex;
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity ${time}ms ease-in-out;
+  `;
+
+  const closeModal = event => {
+    const target = event.target;
+
+    if (
+      target === modalElem ||
+      (btnClose && target.closest(btnClose)) ||
+      event.code === 'Escape'
+      ) {
+      
+      modalElem.style.opacity = 0;
+
+      setTimeout(() => {
+        modalElem.style.visibility = 'hidden';
+        scrollController.enabledScroll();
+      }, time);
+
+      window.removeEventListener('keydown', closeModal);
+    }
+  }
+
+  const openModal = () => {
+    modalElem.style.visibility = 'visible';
+    modalElem.style.opacity = 1;
+    window.addEventListener('keydown', closeModal);
+    scrollController.disabledScroll();
+  };
+
+  buttonElems.forEach(btn => {
+    btn.addEventListener('click', openModal);
+  });
+
+  modalElem.addEventListener('click', closeModal);
+};
+
+modalController({
+  modal: '.modal',
+  btnOpen: '.button_o',
+  btnClose: '.modal__close',
 });
